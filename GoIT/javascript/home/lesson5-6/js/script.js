a0e63f1a04a89b8341b5297c85f4009ee86770c2
IraPristupa <Tetya_Ira@bigmir.net>
2015-12-29 20:11:48 +0300

    addLesson5-6

--- GoIT/javascript/home/lesson5-6/js/script.js
+++ GoIT/javascript/home/lesson5-6/js/script.js
@@ -13,13 +13,13 @@ var Timer = function() {
 }
 
 Timer.prototype.startTimer = function() {  
-  this.toggleElement('st', 'Pause', 't.pauseTimer()')
+  this.toggleElement('st', 'Pause', 'timer.pauseTimer()')
 
   this.setInterval('hours', 3600000)
   this.setInterval('minutes', 60000)
   this.setInterval('seconds', 1000)
   this.setInterval('ms', 1)
-}
+};
 
 Timer.prototype.stopTimer = function() {
     this.hours = 0;
@@ -28,34 +28,42 @@ Timer.prototype.stopTimer = function() {
     this.ms = 0;
   
   for(var t in this.timers) {
-    document.getElementById(t).innerHTML = '00'
-  }
-  this.toggleElement('st', 'Start', 't.startTimer()')
-}
+    document.getElementsByClassName(t)[0].innerHTML = '00'
+    clearInterval(this.timers[t])
+  };
+  this.toggleElement('st', 'Start', 'timer.startTimer()')
+};
 
 Timer.prototype.pauseTimer = function() {
-  this.toggleElement('st', 'Cont..', 't.startTimer()')
+  this.toggleElement('st', 'Cont...', 'timer.startTimer()')
    for(var t in this.timers) {
     clearInterval(this.timers[t])
-  }
-}
+  };
+};
 
 Timer.prototype.setInterval = function(elementName, delay) {
   var self = this
   this.timers[elementName] = setInterval(function() {
      var result = ++self[elementName]
+     if(elementName == 'ms' && self[elementName] > 1000) {
+        self[elementName] = 0
+        result = 0
+     }
+     if(elementName != 'ms' && self[elementName] >= 60){
+        self[elementName] = 0
+        result = 0
+     }
      if(result < 10) {
        result = '0'+self[elementName]
      }
-     document.getElementById(elementName).innerHTML = result
+     document.getElementsByClassName(elementName)[0].innerHTML = result
   }, delay)
-}
+};
 
 Timer.prototype.toggleElement = function(element, text, fn) {
   var e = document.getElementById(element)
   e.innerHTML = text
   e.setAttribute('onclick', fn)
-}
+};
 
-
-var t = new Timer()
+var timer = new Timer();
