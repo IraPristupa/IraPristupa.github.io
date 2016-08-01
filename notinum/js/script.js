e3a87fbfc2025c0cd0f7091dd9198798d49a670a
IraPristupa <Tetya_Ira@bigmir.net>
2016-08-01 19:27:53 +0300

    adding js

--- notinum/js/script.js
+++ notinum/js/script.js
@@ -1,27 +1,41 @@
 /* --------------------- SLIDER ----------------------- */
 
 $(document).ready(function() {
+	var sliderCount = $('.slider li').length;
  $(".slider").each(function () { // обрабатываем каждый слайдер
   var obj = $(this);
-  // $(obj).append("<div class='nav'></div>");
- //  $(obj).find("li").each(function () {
- //   $(obj).find(".prev").append("<span rel='"+$(this).index()+"'></span>"); // добавляем блок навигации
- //   $(this).addClass("slider"+$(this).index());
- //  });
- //  $(obj).find("span").first().addClass("on"); // делаем активным первый элемент меню
- // });
-// });
-function sliderJS (obj, sl) { // slider function
- var ul = $(sl).find("ul"); // находим блок
+  $(obj).find("li").each(function () {
+  	if($(this).index() == 0) { $(this).addClass('active') }
+   $(this).addClass("slider"+$(this).index());
+  });
+  $(obj).find("span").first().addClass("on"); // делаем активным первый элемент меню
+ });
+});
+function sliderJS (obj) { // slider function
+ var sl = $('.slider');
+ var ul = sl.find("ul"); // находим блок
  var bl = $(sl).find("li.slider"+obj); // находим любой из элементов блока
  var step = $(bl).width(); // ширина объекта
+ sl.find('.active').removeClass('active')
+ bl.addClass('active')
  $(ul).animate({marginLeft: "-"+step*obj}, 500); // 500 это скорость перемотки
+
 }
-$(document).on("click", ".slider .nav span", function() { // slider click navigate
- var sl = $(this).closest(".slider"); // находим, в каком блоке был клик
- $(sl).find("span").removeClass("on"); // убираем активный элемент
- $(this).addClass("on"); // делаем активным текущий
- var obj = $(this).attr("rel"); // узнаем его номер
- sliderJS(obj, sl); // слайдим
- return false;
+$(document).on("click", ".slider-box i[class*=fa-arrow-]", function() { // slider click navigate
+ var sl = $('.slider .active');
+ var direction = $(this).data('direction');
+
+ if(direction == 'next') {
+ 	var index = parseInt(sl.index()+1);
+ 	var next = $('.slider'+index);
+ 	if(next.length) {
+ 		sliderJS(index);
+ 	}
+ } else {
+ 	var index = parseInt(sl.index() - 1);
+ 	var prev = $('.slider'+index);
+ 	if(prev.length) {
+ 		sliderJS(index);
+ 	}
+ }
 });
