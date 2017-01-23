e39bba0d404f80824702ca977a715b2fab639b55
IraPristupa <Tetya_Ira@bigmir.net>
2017-01-23 21:54:51 +0300

    styles for ie8

--- project/js/script.js
+++ project/js/script.js
@@ -1,31 +1,20 @@
-// ------------------------  slider -----------------------
-
-$(function() {
-	$('.carousel').flickity({
-		pageDots: false,
-		wrapAround: true,
-		prevNextButtons: true,
-		autoPlay: true,
-		lazyLoad: true
-	});
-});
 
 // ------------   dropdown menu/ sub-menu --------------------
 
 $(function() {
-	var $dropdown = $('.dropdown');
+    var $dropdown = $('.dropdown');
 
-	$dropdown.hover(function() {
-	  $( this ).fadeIn( 300 ).stop(true);
-	  $( this ).fadeOut( 200 ).stop(true);
-	  
-	  var $submenu = $(this).children().siblings('.sub-menu');
-	  var color = Math.floor(Math.random() * (999999 - 100000) + 100000);
-	    $submenu.slideToggle();
-	    $submenu.css('background', '#'+color)
-	    });
+    $dropdown.hover(function() {
+      $( this ).fadeIn( 300 ).stop(true);
+      $( this ).fadeOut( 200 ).stop(true);
+      
+      var $submenu = $(this).children().siblings('.sub-menu');
+      var color = Math.floor(Math.random() * (999999 - 100000) + 100000);
+        $submenu.slideToggle();
+        $submenu.css('background', '#'+color)
+        });
 
-})
+});
 
 // --------------------- pagination ---------------------------
 
@@ -45,11 +34,24 @@ $(function() {
         cssStyle: "light-theme",
         onPageClick: function(pageNumber) { // this is where the magic happens
         // someone changed page, lets hide/show trs appropriately
-        	var showFrom = perPage * (pageNumber - 1);
-        	var showTo = showFrom + perPage;
+            var showFrom = perPage * (pageNumber - 1);
+            var showTo = showFrom + perPage;
 
-        	items.hide() // first hide everything, then show for the new page
+            items.hide() // first hide everything, then show for the new page
                  .slice(showFrom, showTo).show();
         }
     });
 });
+
+// ------------------------  slider -----------------------
+
+$(function() {
+    $('.carousel').flickity({
+        pageDots: false,
+        wrapAround: true,
+        prevNextButtons: true,
+        autoPlay: true,
+        lazyLoad: true
+    });
+});
+
