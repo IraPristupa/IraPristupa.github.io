1ba57f64c720381603a2bfb7577de311d7a73bfe
IraPristupa <Tetya_Ira@bigmir.net>
2017-01-25 18:01:41 +0300

    fix files liocation

---
+++ restaurant/src/js/menus.js
@@ -0,0 +1,124 @@
+var menus = [
+    {
+        title: 'Dish 1',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 2',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 3',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 4',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 5',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 6',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 7',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 8',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 9',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 10',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 11',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 12',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 13',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 14',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 15',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 16',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 17',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 18',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 19',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 20',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 21',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 22',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 23',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 24',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 25',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 26',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 27',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 28',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 29',
+        description: 'chargrilled sourdough bread, wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    },
+    {
+        title: 'Dish 30',
+        description: 'wedge of rye &amp; caraway bread and French baguette with dIsigny butter'
+    }
+];
+
+document.getElementById('main-menu').innerHTML = window.Templates.Menus(menus);
